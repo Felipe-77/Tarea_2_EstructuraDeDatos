1eb47a058337e722a467e8ba7efbab3250e6e9f6
ManuelsAguilera <maguileraespejo@gmail.com>
2023-04-24 11:05:00 -0400

    hola

--- repo_update.sh
+++ repo_update.sh
@@ -1,28 +1,15 @@
 #! /bin/bash
 
-# A script to quickly create a branch, add a commit, and push it up.
+#The idea is to use this script in a git cloned repo
+#because then git will be already set
 
-#set -eo pipefail
-
-current_branch=$(git rev-parse --abbrev-ref HEAD)
-
-if [ "$current_branch" != master ] && [ "$current_branch" != main ]; then
-  echo WARNING: YOU BRANCHED OF OFF "$current_branch", not main or master.
-fi
-
-if [ -z "$1" ]; then
-  echo ERROR: Must have description
-  exit 1
-fi
+printf "Añade un comentario\n"
 
 
-description=$1                                                              # Fix duplication bug
-kebab_name=$(echo "$description" | tr '[:upper:]' '[:lower:]' | tr ' ' '-') # fix-duplication-bug
-commit_message="${description}"                            # ENG-123 Fix duplication bug
 
-git checkout -- 
-git add .
-git status
-git commit -m "$commit_message"
-git push 
-git pull
+git add *
+git commit -m "$1"
+
+printf "\nLa contraseña es tu token de acceso\n"
+git push
+
